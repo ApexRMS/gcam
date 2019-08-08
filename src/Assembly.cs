b2bde17a4da9d1290918f248b3644b63410eb6cd
alex <alexe_webmail@yahoo.com>
2019-08-08 11:52:16 -0700

    Add basic infrastructure for GCAM package.

---
+++ src/Assembly.cs
@@ -0,0 +1,6 @@
+﻿// gcam: SyncroSim Base Package for the Global Change Assessment Model (GCAM).
+// Copyright © 2007-2019 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
+
+using System;
+
+[assembly: CLSCompliant(true)]
