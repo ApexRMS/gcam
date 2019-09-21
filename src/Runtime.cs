2a7b198fb2ec2c23e0bcba1e336c671ce99eac03
alex <alexe_webmail@yahoo.com>
2019-09-20 18:08:42 -0700

    Added GCAM output parsing code to runtime.

--- src/Runtime.cs
+++ src/Runtime.cs
@@ -13,22 +13,35 @@ namespace SyncroSim.GCAM
 {
     class Runtime : Transformer
     {
+        private DataSheet m_StratumDataSheet;
+        private DataSheet m_StateLabelXDataSheet;
+        private DataSheet m_StateLabelYDataSheet;
+        private DataSheet m_StateLabelZDataSheet;
+
         public override void Transform()
         {
             StringDictionary env = new StringDictionary();
             env.Add("SSIM_USER_INTERACTIVE", "True");
-                
-            string RunModelBatchFileName = this.CreateRunGCAMBatchFile();
-            this.ExecuteProcess(RunModelBatchFileName, null, false, env);
 
+            this.InitDataSheets();
             this.ConfigureRunControl();
 
+            string RunModelBatchFileName = this.CreateRunGCAMBatchFile();
             string DetailedLandAllocationQuery = this.CreateDetailedLandAllocationQuery();
             string DetailedLandAllocationBatchFileName = this.CreateDetailedLandAllocationBatchFile(DetailedLandAllocationQuery);
             string DetailedLandAllocationCMDFileName = this.CreateDetailedLandAllocationCMDFile(DetailedLandAllocationBatchFileName);
 
+            this.ExecuteProcess(RunModelBatchFileName, null, false, env);
             this.ExecuteProcess(DetailedLandAllocationCMDFileName, null, false, env);
-            this.ImportGCAMOutput();
+            this.ImportDetailedLandAllocationData();
+        }
+
+        private void InitDataSheets()
+        {
+            this.m_StratumDataSheet = this.Project.GetDataSheet(Shared.STRATUM_DATASHEET_NAME);
+            this.m_StateLabelXDataSheet = this.Project.GetDataSheet(Shared.STATE_LABEL_X_DATASHEET_NAME);
+            this.m_StateLabelYDataSheet = this.Project.GetDataSheet(Shared.STATE_LABEL_Y_DATASHEET_NAME);
+            this.m_StateLabelZDataSheet = this.Project.GetDataSheet(Shared.STATE_LABEL_Z_DATASHEET_NAME);
         }
 
         private string GetGCAMFolderName()
@@ -66,7 +79,7 @@ namespace SyncroSim.GCAM
 
         private string GetUserInputFileName(string columnName)
         {
-            DataSheet ds = this.ResultScenario.GetDataSheet(Shared.INPUT_FILE_DATAFEED_NAME);
+            DataSheet ds = this.ResultScenario.GetDataSheet(Shared.INPUT_FILE_DATASHEET_NAME);
             string ColumnTitle = ds.Columns[columnName].DisplayName;
             DataRow dr = ds.GetDataRow();
 
@@ -136,10 +149,10 @@ namespace SyncroSim.GCAM
             XElement StartYearElement = ModelTimeElement.Element("start-year");
             XElement EndYearElement = ModelTimeElement.Element("end-year");
 
-            DataSheet ds = this.ResultScenario.GetDataSheet(Shared.RUN_CONTROL_DATAFEED_NAME);
+            DataSheet ds = this.ResultScenario.GetDataSheet(Shared.RUN_CONTROL_DATASHEET_NAME);
             DataRow dr = ds.GetData().NewRow();
 
-            dr[Shared.RUN_CONTROL_MIN_ITERATION_COLUMN_NAME] = "0";
+            dr[Shared.RUN_CONTROL_MIN_ITERATION_COLUMN_NAME] = "1";
             dr[Shared.RUN_CONTROL_MAX_ITERATION_COLUMN_NAME] = "1";
             dr[Shared.RUN_CONTROL_MIN_TIMESTEP_COLUMN_NAME] = Convert.ToInt32(StartYearElement.Value.ToString());
             dr[Shared.RUN_CONTROL_MAX_TIMESTEP_COLUMN_NAME] = Convert.ToInt32(EndYearElement.Value.ToString());
@@ -261,10 +274,206 @@ namespace SyncroSim.GCAM
             return CMDFileName;
         }
 
-        private void ImportGCAMOutput()
+        private void ImportDetailedLandAllocationData()
         {
             string GCAMOutputFolder = this.GetGCAMOutputFolderName();
+            string CSVFileName = Path.Combine(GCAMOutputFolder, "detailed_land_allocation.csv");
 
+            if (!File.Exists(CSVFileName))
+            {
+                throw new DataException("Cannot find the detailed land allocation data: " + CSVFileName);
+            }
+
+            //Example CSV file:
+
+            //detailed land allocation
+            //scenario, region, LandLeaf,1990,2005,2010,2015,2020,2025,2030,2035,2040,2045,2050,2055,2060,2065,2070,2075,2080,2085,2090,2095,2100,Units,
+            //"GCAM-USA_Ref,date=2019-20-9T13:48:08-07:00",USA,Corn_ArkWhtRedR_IRR_hi,2.40186,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,thous km2,
+            //"GCAM-USA_Ref,date=2019-20-9T13:48:08-07:00",USA,Corn_ArkWhtRedR_IRR_lo,2.40186,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,thous km2,
+            //"GCAM-USA_Ref,date=2019-20-9T13:48:08-07:00",USA,Corn_ArkWhtRedR_RFD_hi,1.33969,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,thous km2,
+
+            DataSheet ds = this.ResultScenario.GetDataSheet(Shared.DETAILED_LAND_ALLOCATION_DATASHEET_NAME);
+            DataTable dt = ds.GetData();
+
+            using (StreamReader s = new StreamReader(CSVFileName))
+            {
+                string line;
+                string[] TimestepsLineSplit;
+
+                line = s.ReadLine(); //Skip title
+                TimestepsLineSplit = s.ReadLine().Split(',');
+
+                int FirstTimestepIndex = 3;
+                int LastTimestepIndex = TimestepsLineSplit.Length - 3; //The last column is the units and there is a comma at the end of each line...
+
+                while ((line = s.ReadLine()) != null)
+                {
+                    string[] LineValues = line.Split(',');
+                    string LandLeaf = LineValues[3];  //3 because there is a comma in the scenario name
+
+                    string StratumName = null;
+                    string StateLabelXName = null;
+                    string StateLabelYName = null;
+                    string StateLabelZName = null;
+
+                    this.ParseLandLeafNames(
+                        LandLeaf, 
+                        ref StratumName, 
+                        ref StateLabelXName, 
+                        ref StateLabelYName,
+                        ref StateLabelZName);
+
+                    object StratumID = null;
+                    object StateLabelXID = null;
+                    object StateLabelYID = null;
+                    object StateLabelZID = null;
+
+                    if (!GetStratumId(StratumName, ref StratumID) ||
+                        !GetStateLabelXID(StateLabelXName, ref StateLabelXID) ||
+                        !GetStateLabelYID(StateLabelYName, ref StateLabelYID) ||
+                        !GetStateLabelZID(StateLabelZName, ref StateLabelZID))
+                    {
+                        continue;
+                    }
+
+                    for (int i = FirstTimestepIndex; i <= LastTimestepIndex; i++)
+                    {
+                        object Timestep = TimestepsLineSplit[i];
+                        object Amount = LineValues[i + 1]; //Shifted by one because there is a comma in the scenario name
+
+                        DataRow dr = dt.NewRow();
+
+                        dr[Shared.DETAILED_LAND_ALLOCATION_ITERATION_COLUMN_NAME] = 1;
+                        dr[Shared.DETAILED_LAND_ALLOCATION_TIMESTEP_COLUMN_NAME] = Timestep;
+                        dr[Shared.DETAILED_LAND_ALLOCATION_STRATUMID_COLUMN_NAME] = StratumID;
+                        dr[Shared.DETAILED_LAND_ALLOCATION_STATE_LABEL_X_ID_COLUMN_NAME] = StateLabelXID;
+                        dr[Shared.DETAILED_LAND_ALLOCATION_STATE_LABEL_Y_ID_COLUMN_NAME] = StateLabelYID;
+                        dr[Shared.DETAILED_LAND_ALLOCATION_STATE_LABEL_Z_ID_COLUMN_NAME] = StateLabelZID;
+                        dr[Shared.DETAILED_LAND_ALLOCATION_AMOUNT_COLUMN_NAME] = Amount;
+
+                        dt.Rows.Add(dr);
+                    }
+                }
+            }
+        }
+
+        private void ParseLandLeafNames(
+            string landLeaf, 
+            ref string stratumName, 
+            ref string stateLabelXName, 
+            ref string stateLabelYName, 
+            ref string stateLabelZName)
+        {
+            //Example LandLeaf Values: 
+
+            //Corn_ArkWhtRedR_IRR_hi
+            //biomass_tree_ArkWhtRedR_RFD_lo
+            //Forest_ArkWhtRedR
+
+            //Stratum........... ArkWhtRedR
+            //StateLabelX....... Corn
+            //StateLabelY....... IRR
+            //StateLabelZ....... hi
+
+            //Note that there can be either 1, 3, or 4 separators...
+
+            stateLabelYName = null; //can be null
+            stateLabelZName = null; //can be null
+
+            string[] LandLeafValues = landLeaf.Split('_');
+
+            if (LandLeafValues.Length != 2 &&
+                LandLeafValues.Length != 4 &&
+                LandLeafValues.Length != 5)
+            {
+                throw new DataException("Cannot parse LandLeaf: " + landLeaf);
+            }
+
+            if (LandLeafValues.Length == 2)
+            {
+                stratumName = LandLeafValues[1];
+                stateLabelXName = LandLeafValues[0];
+            }
+            else if (LandLeafValues.Length == 4)
+            {
+                stratumName = LandLeafValues[1];
+                stateLabelXName = LandLeafValues[0];
+                stateLabelYName = LandLeafValues[2];
+                stateLabelZName = LandLeafValues[3];
+            }
+            else
+            {
+                stratumName = LandLeafValues[2];
+                stateLabelXName = LandLeafValues[0] + "_" + LandLeafValues[1];
+                stateLabelYName = LandLeafValues[3];
+                stateLabelZName = LandLeafValues[4];
+            }
+        }
+
+        private bool GetStratumId(string name, ref object id)
+        {
+            if (this.m_StratumDataSheet.ValidationTable.ContainsValue(name))
+            {
+                id = this.m_StratumDataSheet.ValidationTable.GetValue(name);
+                return true;
+            }
+            else
+            {
+                this.RecordStatus(StatusType.Warning, "Import data contains missing Stratum: " + name);
+                return false;
+            }
+        }
+
+        private bool GetStateLabelXID(string name, ref object id)
+        {
+            if (this.m_StateLabelXDataSheet.ValidationTable.ContainsValue(name))
+            {
+                id = this.m_StateLabelXDataSheet.ValidationTable.GetValue(name);
+                return true;
+            }
+            else
+            {
+                this.RecordStatus(StatusType.Warning, "Import data contains missing State Label X: " + name);
+                return false;
+            }
+        }
+
+        private bool GetStateLabelYID(string name, ref object id)
+        {
+            if (name == null)
+            {
+                id = DBNull.Value;
+                return true;
+            }
+            else if (this.m_StateLabelYDataSheet.ValidationTable.ContainsValue(name))
+            {
+                id = this.m_StateLabelYDataSheet.ValidationTable.GetValue(name);
+                return true;
+            }
+            else
+            {
+                this.RecordStatus(StatusType.Warning, "Import data contains missing State Label Y: " + name);
+                return false;
+            }
+        }
+
+        private bool GetStateLabelZID(string name, ref object id)
+        {
+            if (name == null)
+            {
+                id = DBNull.Value;
+                return true;
+            }
+            else if (this.m_StateLabelZDataSheet.ValidationTable.ContainsValue(name))
+            {
+                id = this.m_StateLabelZDataSheet.ValidationTable.GetValue(name);
+                return true;
+            }
+            else
+            {
+                this.RecordStatus(StatusType.Warning, "Import data contains missing State Label Z: " + name);
+                return false;
+            }
         }
     }
 }
--- src/Shared.cs
+++ src/Shared.cs
@@ -5,26 +5,31 @@ namespace SyncroSim.GCAM
 {
     static class Shared
     {
-        public static string APPLICATION_DATAFEED_NAME = "gcam_Application";
-        public static string APPLICATION_FOLDER_COLUMN_NAME = "Directory";
+        public const string APPLICATION_DATAFEED_NAME = "gcam_Application";
+        public const string APPLICATION_FOLDER_COLUMN_NAME = "Directory";
 
-        public static string RUN_CONTROL_DATAFEED_NAME = "gcam_RunControl";
+        public const string STRATUM_DATASHEET_NAME = "gcam_Stratum";
+        public const string STATE_LABEL_X_DATASHEET_NAME = "gcam_StateLabelX";
+        public const string STATE_LABEL_Y_DATASHEET_NAME = "gcam_StateLabelY";
+        public const string STATE_LABEL_Z_DATASHEET_NAME = "gcam_StateLabelZ";
+
+        public const string RUN_CONTROL_DATASHEET_NAME = "gcam_RunControl";
         public const string RUN_CONTROL_MIN_ITERATION_COLUMN_NAME = "MinimumIteration";
         public const string RUN_CONTROL_MAX_ITERATION_COLUMN_NAME = "MaximumIteration";
         public const string RUN_CONTROL_MIN_TIMESTEP_COLUMN_NAME = "MinimumTimestep";
         public const string RUN_CONTROL_MAX_TIMESTEP_COLUMN_NAME = "MaximumTimestep";
 
-        public static string INPUT_FILE_DATAFEED_NAME = "gcam_InputFile";
-        public static string INPUT_FILE_CONFIGURATION_FILE_COLUMN_NAME = "ConfigurationFile";      
-        public static string INPUT_FILE_POLICY_TARGET_FILE_COLUMN_NAME = "PolicyTargetFile"; 
+        public const string INPUT_FILE_DATASHEET_NAME = "gcam_InputFile";
+        public const string INPUT_FILE_CONFIGURATION_FILE_COLUMN_NAME = "ConfigurationFile";      
+        public const string INPUT_FILE_POLICY_TARGET_FILE_COLUMN_NAME = "PolicyTargetFile"; 
         
-        public static string DETAILED_LAND_ALLOCATION_DATAFEED_NAME = "gcam_DetailedLandAllocation";
-        public static string DETAILED_LAND_ALLOCATION_ITERATION_COLUMN_NAME = "Iteration";
-        public static string DETAILED_LAND_ALLOCATION_TIMESTEP_COLUMN_NAME = "Timestep";
-        public static string DETAILED_LAND_ALLOCATION_STRATUMID_COLUMN_NAME = "StratumID";
-        public static string DETAILED_LAND_ALLOCATION_STATE_LABEL_X_ID_COLUMN_NAME = "StateLabelXID";
-        public static string DETAILED_LAND_ALLOCATION_STATE_LABEL_Y_ID_COLUMN_NAME = "StateLabelYID";
-        public static string DETAILED_LAND_ALLOCATION_STATE_LABEL_Z_ID_COLUMN_NAME = "StateLabelZID";
-        public static string DETAILED_LAND_ALLOCATION_AMOUNT_COLUMN_NAME = "Amount";
+        public const string DETAILED_LAND_ALLOCATION_DATASHEET_NAME = "gcam_DetailedLandAllocation";
+        public const string DETAILED_LAND_ALLOCATION_ITERATION_COLUMN_NAME = "Iteration";
+        public const string DETAILED_LAND_ALLOCATION_TIMESTEP_COLUMN_NAME = "Timestep";
+        public const string DETAILED_LAND_ALLOCATION_STRATUMID_COLUMN_NAME = "StratumID";
+        public const string DETAILED_LAND_ALLOCATION_STATE_LABEL_X_ID_COLUMN_NAME = "StateLabelXID";
+        public const string DETAILED_LAND_ALLOCATION_STATE_LABEL_Y_ID_COLUMN_NAME = "StateLabelYID";
+        public const string DETAILED_LAND_ALLOCATION_STATE_LABEL_Z_ID_COLUMN_NAME = "StateLabelZID";
+        public const string DETAILED_LAND_ALLOCATION_AMOUNT_COLUMN_NAME = "Amount";
     }
 }
